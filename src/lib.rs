eed50902e65f3e68ca54c996c475eb5e3b06aab7
Wesley Moore <wes@wezm.net>
2022-01-21 16:42:08 +1000

    Update tests

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //! ```
 //! // U+0307 is COMBINING DOT ABOVE
 //! assert_eq!(unicode_case_mapping::to_lowercase('İ'), ['i' as u32, 0x0307]);
-//! assert_eq!(unicode_case_mapping::to_lowercase('ß'), [0; 2]);
+//! assert_eq!(unicode_case_mapping::to_lowercase('ß'), ['ß' as u32, 0]);
 //! assert_eq!(unicode_case_mapping::to_uppercase('ß'), ['S' as u32, 'S' as u32, 0]);
 //! assert_eq!(unicode_case_mapping::to_titlecase('ß'), ['S' as u32, 's' as u32, 0]);
 //! assert_eq!(unicode_case_mapping::to_titlecase('-'), [0; 3]);
